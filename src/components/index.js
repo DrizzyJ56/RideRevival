0d115f50da70484e3ad0c58369671d4d5f5d1ab0
DrizzyJ56 <blevins.j921@gmail.com>
2023-11-13 11:37:43 -0500

    removing unused  /allProducts from index

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import Home from './Home';
 import Coating from './Coating';
-import AllProducts from './AllProducts';
 import Footer from './Footer';
 import OtherNav from './OtherNav';
 import Services from './Services';
@@ -13,7 +12,6 @@ export {
   Hero,
   Home,
   Coating,
-  AllProducts,
   Footer,
   OtherNav,
   Services,
