458d5dd3db64ea81606e225ad4ca3969de463b85
DrizzyJ56 <blevins.j921@gmail.com>
2023-11-14 12:24:41 -0500

    update to /about again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,13 @@ import { danielLongSignature } from "../assets";
 const About = () => {
   return (
     <div className="min-h-screen bg-black text-slate-300">
-      <div className="relative flex flex-col text-center justify-center items-center pt-40">
+      <div className="relative flex flex-col text-center justify-center items-center pt-40 sm:pt-28 xs:pt-28">
         <h3 className="text-8xl sm:text-4xl xs:text-4xl font-bold">
           Ride
           <span className="underline underline-offset-[2vh]">Revival Fou</span>
           nder
         </h3>
-        <p className="text-4xl ml-24 mr-24 mt-16 mb-8 sm:text-2xl xs:text-2xl">
+        <p className="text-4xl ml-24 mr-24 mt-16 mb- sm:mt-8 xs:mt-8 sm:text-2xl xs:text-2xl">
           I launched Ride Revival in 2023 after years of detailing as a hobby.
           My first car was a Mustang that was rough around the edges. I spent
           countless hours trying to perfect that car from the moment it got
